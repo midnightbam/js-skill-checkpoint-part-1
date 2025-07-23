c7c374e3b57afac66aae1df73e694d72b471359b
Bambam <bam.ksnru@gmail.com>
2025-07-23 14:55:47 +0700

    feat: add answer to javascript skill checkpoint1

--- question-2.js
+++ question-2.js
@@ -4,3 +4,14 @@ const inventory = [
   { name: "Banana", price: 10, quantity: 50 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+inventory[0].quantity= 200;
+
+inventory.push({name: "Orange", price: 20, quantity: 300});
+
+let totalPrice = 0;
+for(let item of inventory){
+  totalPrice += item.price * item.quantity;
+}
+console.log(`มูลค่ารวมของจำนวนสินค้าทั้งหมดในสต็อก ${totalPrice} บาท`);
+  
+
